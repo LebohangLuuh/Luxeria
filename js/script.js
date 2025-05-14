cbe070dda6aa5fd51a53b92b6b0cf5039c1c4f2f
Lebohang Mofokeng <lp.mofokeng96@gmail.com>
2025-05-14 14:25:07 +0200

    rendered products with filter

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Product fetching and validation
+async function fetchProducts() {
+    try {
+      const response = await fetch("https://dummyjson.com/products");
+      if (!response.ok) throw new Error("Failed to fetch products");
+      const { products } = await response.json();
+      return products;
+    } catch (error) {
+      throw new Error(`Product Service Error: ${error.message}`);
+    }
+  }
+  
+  function findProduct(products, productId) {
+    return products.find((p) => p.id === parseInt(productId));
+  }
+
+  //filter product by category
+function filterProduct(products, category) {
+    if (!category) return products; 
+    return products.filter(product => 
+      product.category && product.category.toLowerCase() === category.toLowerCase()
+    );
+  }
+
+  export {
+    fetchProducts,
+    findProduct,
+    filterProduct
+  };
